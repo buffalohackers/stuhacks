f5aeaf1548d4690335a5f4a5603085839035c111
Nick DiRienzo <nick@dirienzo.co>
2013-04-10 05:20:43 -0400

    really crappy version of getting all the student hackathons and listing them

--- stuhacks.py
+++ stuhacks.py
@@ -1,8 +1,24 @@
+import json
 import os
+
 import flask
+import requests
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-	return "hey."
+	resp = dict()
+	hackathons = [0]
+	p = {'limit': 100, 'offset': 0}
+	while hackathons:
+		r = requests.get('https://www.hackerleague.org/api/v1/hackathons.json', params=p)
+		hackathons = r.json()
+		for hackathon in hackathons:
+			if hackathon['students_only']:
+				resp[hackathon['id']] = hackathon['name']
+		p['offset'] += 1
+	return '<br/>'.join(resp.values())
+
+if __name__ == "__main__":
+	app.run()
