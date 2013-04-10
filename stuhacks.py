0c0d6ee9abe97aadfe809cc81389b046c42af804
Nick DiRienzo <nick@dirienzo.co>
2013-04-10 18:49:20 -0400

    Sort off start_time instead of just reversing the list. Stop searching once the first complete hackathon appears.

--- stuhacks.py
+++ stuhacks.py
@@ -10,22 +10,24 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-	resp_hackathons = list()
-	hackathons = [0]
-	p = {'limit': 100, 'offset': 0}
-	while hackathons:
-		r = requests.get('https://www.hackerleague.org/api/v1/hackathons.json', params=p)
-		hackathons = r.json()
-		for hackathon in hackathons:
-			if hackathon['students_only'] and hackathon['state'] != 'complete':
-				resp_hackathons.append(hackathon)
-		p['offset'] += 1
-	resp_hackathons.reverse()
-	return flask.render_template('index.html', hackathons=resp_hackathons)
+    resp_hackathons = list()
+    complete = False
+    p = {'limit': 100, 'offset': 0}
+    while not complete:
+        r = requests.get('https://www.hackerleague.org/api/v1/hackathons.json', params=p)
+        hackathons = r.json()
+        for hackathon in hackathons:
+            if hackathon['students_only'] and hackathon['state'] != 'complete':
+                resp_hackathons.append(hackathon)
+            elif hackathon['state'] == 'complete':
+                complete = True
+        p['offset'] += 1
+    resp_hackathons = sorted(resp_hackathons, key=lambda x: x['start_time'])
+    return flask.render_template('index.html', hackathons=resp_hackathons)
 
 @app.route('/about')
 def about():
-	return flask.render_template('about.html')
+    return flask.render_template('about.html')
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
